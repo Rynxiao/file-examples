a23f8bfd0efa6b1be319420a4e8e26c35ed2920c
rynxiao <yuzhongzi91@sina.com>
2021-06-19 00:32:07 +0800

    feat: generate file hash

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -2,36 +2,60 @@ const $fileUpload = document.getElementById('fileUpload');
 
 const CHUNK_SIZE = 1024 * 1024 * 0.2;
 
-const slice = (file, piece = CHUNK_SIZE) => {
-  let totalSize = file.size;
-  let start = 0;
-  let end = start + piece;
-  const chunks = [];
-  while (start < totalSize) {
-    let chunk = file.slice(start, end);
-    chunks.push(chunk);
+const checkSum = (file, piece = CHUNK_SIZE) => {
+  return new Promise((resolve, reject) => {
+    let totalSize = file.size;
+    let start = 0;
+    const chunks = [];
+    const spark = new SparkMD5.ArrayBuffer();
+    const fileReader = new FileReader();
 
-    start = end;
-    end = start + piece;
-  }
-  return chunks;
+    const loadNext = () => {
+      const end = start + piece;
+      const chunk = file.slice(start, end);
+
+      start = end;
+      chunks.push(chunk);
+      fileReader.readAsArrayBuffer(chunk);
+    };
+
+    fileReader.onload = (event) => {
+      const buffer = event.target.result;
+
+      if (start < totalSize) {
+        spark.append(buffer);
+        loadNext();
+      } else {
+        const sum = spark.end();
+        resolve({ chunks, sum });
+      }
+    };
+
+    fileReader.onerror = function () {
+      console.warn('oops, something went wrong.');
+      reject();
+    };
+
+    loadNext();
+  });
 };
 
 $fileUpload.addEventListener('change', (event) => {
   const file = event.target.files[0];
-  const chunks = slice(file);
-  const tasks = [];
+  checkSum(file).then(({ chunks, sum }) => {
+    const tasks = [];
 
-  chunks.forEach((chunk, index) => {
-    const fd = new FormData();
-    fd.append('file', chunk);
-    fd.append('context', file.name);
-    fd.append('chunkIndex', index.toString());
-    tasks.push(axios({ url: '/upload', method: 'post', data: fd }).then((res) => res.data));
-  });
+    chunks.forEach((chunk, index) => {
+      const fd = new FormData();
+      fd.append('file', chunk);
+      fd.append('fileHash', sum);
+      fd.append('chunkIndex', index.toString());
+      tasks.push(axios({ url: '/upload', method: 'post', data: fd }).then((res) => res.data));
+    });
 
-  Promise.all(tasks).then(() => {
-    const filename = file.name;
-    axios({ url: '/makefile', method: 'post', data: { chunks: chunks.length, filename } });
+    Promise.all(tasks).then(() => {
+      const filename = file.name;
+      axios({ url: '/makefile', method: 'post', data: { chunks: chunks.length, filename, fileHash: sum } });
+    });
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,29 @@
 const path = require('path');
 const fs = require('fs');
+const fsPromises = require('fs/promises');
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const uploadPath = path.join(__dirname, '..', '/uploads');
 const uploadTmp = path.join(uploadPath, 'tmp');
 const upload = multer({ dest: uploadTmp });
-const errorHandler = (err) => {
-  if (err) {
-    console.error(err);
-  }
-};
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Upload Examples' });
 });
 
-router.post('/upload', upload.any(), (req, res) => {
+router.post('/upload', upload.any(), async (req, res) => {
   const file = req.files[0];
   const destination = file.destination;
   const path = file.path;
 
   try {
-    const originalFilename = req.body.context;
+    const fileHash = req.body.fileHash;
     const chunkIndex = req.body.chunkIndex;
-    const chunkName = `${chunkIndex}.${originalFilename}.chunk`;
+    const chunkName = `${chunkIndex}.${fileHash}.chunk`;
 
-    fs.renameSync(path, `${destination}/${chunkName}`);
+    await fsPromises.rename(path, `${destination}/${chunkName}`);
     res.json({ code: 200 });
   } catch (e) {
     res.json({ code: 500, message: e.message });
@@ -35,31 +31,30 @@ router.post('/upload', upload.any(), (req, res) => {
   }
 });
 
-router.post('/makefile', (req, res) => {
+router.post('/makefile', async (req, res) => {
   const chunks = req.body.chunks;
   const filename = req.body.filename;
+  const fileHash = req.body.fileHash;
   const path = `${uploadPath}/${filename}`;
   try {
     for (let i = 0; i < chunks; i++) {
-      const file = `${uploadTmp}/${i}.${filename}.chunk`;
-      const content = fs.readFileSync(file);
+      const file = `${uploadTmp}/${i}.${fileHash}.chunk`;
+      const content = await fsPromises.readFile(file);
       try {
-        fs.accessSync(path, fs.constants.F_OK);
-        fs.appendFile(path, content, () => {
-          fs.unlink(file, errorHandler);
-          if (i === chunks - 1) {
-            res.json({ code: 200, message: 'upload success!' });
-          }
-        });
+        await fsPromises.access(path, fs.constants.F_OK);
+        await fsPromises.appendFile(path, content);
+        await fsPromises.unlink(file);
+        if (i === chunks - 1) {
+          res.json({ code: 200, message: `file ${filename} upload success!` });
+        }
       } catch (err) {
-        fs.writeFile(path, content, { flag: 'w+' }, () => {
-          fs.unlink(file, errorHandler);
-        });
+        await fsPromises.writeFile(path, content, { flag: 'w+' });
+        await fsPromises.unlink(file);
       }
     }
   } catch (err) {
-    res.json({ code: 500, message: 'upload failed!' });
     res.status(500);
+    res.json({ code: 500, message: `file ${filename} upload failed!` });
   }
 });
 
