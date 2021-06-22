54eddbd6943bbf2eddfa6b69d63620f24927ca21
rynxiao <yuzhongzi91@sina.com>
2021-06-22 21:25:25 +0800

    feat: update start db script when db container is exists

--- scripts/start-db.sh
+++ scripts/start-db.sh
@@ -5,10 +5,18 @@ CONTAINER_NAME=file-db-dev
 MYSQL_ROOT_PASSWORD=pwd123
 DATA_PATH=${SHELL_FOLDER}/data/mysql
 
-docker run \
---name ${CONTAINER_NAME} \
--v ${DATA_PATH}:/var/lib/mysql \
--e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
--p "3306:3306" \
--d \
-mysql:8
+if [ ! "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=${CONTAINER_NAME})" ]; then
+      # start container if stopped
+      docker start ${CONTAINER_NAME}
+    else
+      # run container
+      docker run \
+      --name ${CONTAINER_NAME} \
+      -v ${DATA_PATH}:/var/lib/mysql \
+      -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+      -p "3306:3306" \
+      -d \
+      mysql:8
+    fi
+fi
